f4fadd68c7e20410072be171f8fbca2c93aafc97
Celso Ribeiro <celsofilho82@gmail.com>
2023-08-15 17:46:48 -0300

    Primeiros Passos

---
+++ kotlin-basico-1/bytebank/src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("Bem vindo ao ByteBank!")
+    val titular: String = "Celso"
+    val numeroConta: Int = 1439
+    var saldo: Double = 0.0
+    saldo = 100.0
+
+    println("Titular: $titular")
+    println("Número da conta: $numeroConta")
+    println("Saldo: $saldo")
+}
